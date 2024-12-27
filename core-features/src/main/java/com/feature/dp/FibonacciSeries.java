9060487a44e893a205743842d6cd5092ba97c819
Adnan Ghazanfar <adnan.ghazanfar@ing.com>
2024-12-27 22:44:53 +0100

    dynamic programming

---
+++ core-features/src/main/java/com/feature/dp/FibonacciSeries.java
@@ -0,0 +1,46 @@
+package com.feature.dp;
+
+import java.util.Arrays;
+
+public class FibonacciSeries {
+
+    public static void main(String[] args) {
+        int n = 45;
+        System.out.println("******* Tabulation *******");
+        long s = System.currentTimeMillis();
+        System.out.println(fibonacci(n));
+        long l = System.currentTimeMillis();
+        System.out.println("Time taken: " + (l-s));
+
+
+        System.out.println("******* Memoization *******");
+        long s2 = System.currentTimeMillis();
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+        dp[1] = 1;
+        System.out.println(fibonacci(n, dp));
+        long l2 = System.currentTimeMillis();
+        System.out.println("Time taken: " + (l2-s2));
+        Arrays.stream(dp).forEach(v -> System.out.print(v + " "));
+    }
+
+    // Tabulation
+    private static int fibonacci(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-2] + dp[i-1];
+        }
+        return dp[n];
+    }
+
+    // Memoization.
+    private static int fibonacci(int n, int[] dp) {
+        if ( n <= 1 ) return n;
+        return dp[n] = (dp[n-2] != -1 ? dp[n-2] :fibonacci( n-2, dp)) + (dp[n-1] != -1 ? dp[n-1] : fibonacci(n-1, dp));
+    }
+
+}
